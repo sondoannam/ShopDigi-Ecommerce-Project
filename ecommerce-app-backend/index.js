1330693453a4b15e0037c897b3321fa60e19f6ef
Son Doan Nam <80660666+sondoannam@users.noreply.github.com>
2023-06-16 22:54:24 +0700

    Update index.js

--- ecommerce-app-backend/index.js
+++ ecommerce-app-backend/index.js
@@ -24,18 +24,24 @@ dbConnect();
 
 const app = express();
 
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://shopdigi-api.onrender.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(cors({
+    origin: "https://shopdigi-api.onrender.com",
+    headers: ["Content-Type"],
+    credentials: true,
+}));
 app.use(morgan('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://shopdigi-api.onrender.com"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
 
 app.use('/api/user', authRouter);
 app.use('/api/product', productRouter);
