cbc8cc35eba674f9cebdb3a7396724326429594f
sondoannam <sondoannam202@gmail.com>
2023-06-16 22:59:24 +0700

    fix mongouri

--- ecommerce-app-backend/index.js
+++ ecommerce-app-backend/index.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://shopdigi-api.onrender.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://shopdigi-api.onrender.com/api/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
